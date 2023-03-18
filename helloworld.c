f96cb17a8701a14a7728b945abfeff7d7565cdca
liangjiajun <liangjiajun@163.com>
2023-03-18 16:24:34 +0800

    add new file

---
+++ helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello world \n");
+	printf("111111 \n");
+	printf("222222 \n");
+	return 0;
+}
---
+++ number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int counter_max = 0;
+	int counter_min = 0;
+	
+	return 0;
+}
